d7b548a19f88eb5d993bce5d3330e00eb24712a1
chenset <4199191@qq.com>
2019-01-09 23:38:15 +0800

    update

--- src/goSpider/main.go
+++ src/goSpider/main.go
@@ -4,11 +4,9 @@ import (
 	"fmt"
 	"log"
 	"time"
-	"goSpider/database"
+	"net/url"
 	"goSpider/helper"
-	"goSpider/dispatcher"
-	"goSpider/project"
-	"goSpider/web"
+	"golang.org/x/net/publicsuffix"
 )
 
 func init() {
@@ -23,10 +21,19 @@ func main() {
 		//pSt.Stop()
 	}()
 
-	database.Bl().ClearAll()                             //todo for test
-	database.Redis().Del(helper.Env().Redis.URLQueueKey) //todo for test
+	//database.Bl().ClearAll()                             //todo for test
+	//database.Redis().Del(helper.Env().Redis.URLQueueKey) //todo for test
 
-	c := &dispatcher.Dispatcher{}
-	c.Run(&project.Www{})
-	web.Server(c, ":666") // http://127.0.0.1:666
+	//c := &dispatcher.Dispatcher{}
+	//c.Run(&project.Www{})
+	//web.Server(c, ":666") // http://127.0.0.1:666
+
+
+	u,_:=url.Parse("http://www.b11a24ij16i.com.cngoods-6461.html/")//fixme
+	fmt.Println(u.Hostname())
+	fmt.Println(publicsuffix.PublicSuffix(u.Hostname()))
+	fmt.Println(helper.TldDomain(u))
+	fmt.Println(u.Hostname())
+
 }
+
