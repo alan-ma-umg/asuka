c4372f9ccb7d9a98853b111aadc1e294e42e5073
chenset <4199191@qq.com>
2019-01-13 15:47:27 +0800

    update

--- src/goSpider/queue/queue_test.go
+++ src/goSpider/queue/queue_test.go
@@ -1,9 +1,9 @@
 package queue
 
 import (
+	"fmt"
 	"log"
 	"testing"
-	"time"
 )
 
 func init() {
@@ -11,19 +11,21 @@ func init() {
 }
 func TestNewQueue(t *testing.T) {
 	q := NewQueue()
+	log.Println(q.EnqueueForFailure("sdfdsfds12", 3))
+	log.Println(q.EnqueueForFailure("sdfdsfds11", 3))
+
+	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
+
+	log.Println(q.EnqueueForFailure("sdfdsfds12", 3))
+	log.Println(q.EnqueueForFailure("sdfdsfds12", 3))
+	log.Println(q.EnqueueForFailure("sdfdsfds12", 3))
+
 	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
 	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
 	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 3))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 2))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 4))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 5))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 6))
-	log.Println(q.EnqueueForFailure("sdfdsfds1", 6))
-	log.Println(q.EnqueueForFailure("sdfdsfds2", 3))
-	time.Sleep(16e9)
+
+	log.Println(q.EnqueueForFailure("sdfdsfds11", 3))
+
+	fmt.Println(q.BlsTestCount)
+	//time.Sleep(16e9)
 }
