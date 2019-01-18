a4b66e2633ab8e9d76addd0bca68369f46cbba7e
chenset <4199191@qq.com>
2019-01-18 22:46:19 +0800

    update

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 
 - 使用 net/http/httptrace
 - 完善失败处理 & sleep机制, 尝试使用PID算法控制
-- 按需关闭并重连新的socks
-- UI加入线性图表
 - 重构
--- src/asuka/spider/spider.go
+++ src/asuka/spider/spider.go
@@ -20,7 +20,9 @@ import (
 	"net"
 	"net/http"
 	"net/http/cookiejar"
+	"net/http/httptrace"
 	"net/http/httputil"
+	"net/textproto"
 	"net/url"
 	"reflect"
 	"strconv"
@@ -101,11 +103,14 @@ type Spider struct {
 	RequestBefore   func(spider *Spider)
 	DownloadFilter  func(spider *Spider, response *http.Response) (bool, error)
 	ProjectThrottle func(spider *Spider)
+
+	httpTrace *httptrace.ClientTrace
 }
 
 func New(t *proxy.Transport, queue *queue.Queue) *Spider {
 	spider := &Spider{Queue: queue, Transport: t, RequestsMap: map[string]*http.Request{}, TimeList: list.New(), TimeLenLimit: 10}
 	spider.updateClient()
+	spider.registerHttpTrace()
 	spiderList = append(spiderList, spider)
 	return spider
 }
@@ -234,6 +239,8 @@ func (spider *Spider) SetRequest(url *url.URL, header *http.Header) *Spider {
 	if spider.CurrentRequest.UserAgent() == "" {
 		spider.CurrentRequest.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/"+strconv.FormatFloat(rand.Float64()*10000, 'f', 3, 64)+" (KHTML, like Gecko) Chrome/71.0."+strconv.FormatFloat(rand.Float64()*10000, 'f', 3, 64)+" Safari/537.36")
 	}
+
+	spider.CurrentRequest = spider.CurrentRequest.WithContext(httptrace.WithClientTrace(spider.CurrentRequest.Context(), spider.httpTrace))
 	return spider
 }
 
@@ -575,5 +582,125 @@ func (spider *Spider) GetLinksByTokenizer() (res []*url.URL) {
 		case html.ErrorToken:
 			return
 		}
+	}
+}
+
+func (spider *Spider) registerHttpTrace() {
+	spider.httpTrace = &httptrace.ClientTrace{
+		// GetConn is called before a connection is created or
+		// retrieved from an idle pool. The hostPort is the
+		// "host:port" of the target or proxy. GetConn is called even
+		// if there's already an idle cached connection available.
+		GetConn: func(hostPort string) {
+
+		},
+
+		// GotConn is called after a successful connection is
+		// obtained. There is no hook for failure to obtain a
+		// connection; instead, use the error from
+		// Transport.RoundTrip.
+		GotConn: func(connInfo httptrace.GotConnInfo) {
+			//log.Printf("Got Conn: %+v\n", connInfo)
+		},
+
+		// PutIdleConn is called when the connection is returned to
+		// the idle pool. If err is nil, the connection was
+		// successfully returned to the idle pool. If err is non-nil,
+		// it describes why not. PutIdleConn is not called if
+		// connection reuse is disabled via Transport.DisableKeepAlives.
+		// PutIdleConn is called before the caller's Response.Body.Close
+		// call returns.
+		// For HTTP/2, this hook is not currently used.
+		PutIdleConn: func(err error) {
+
+		},
+
+		// GotFirstResponseByte is called when the first byte of the response
+		// headers is available.
+		GotFirstResponseByte: func() {
+
+		},
+
+		// Got100Continue is called if the server replies with a "100
+		// Continue" response.
+		Got100Continue: func() {
+
+		},
+
+		// Got1xxResponse is called for each 1xx informational response header
+		// returned before the final non-1xx response. Got1xxResponse is called
+		// for "100 Continue" responses, even if Got100Continue is also defined.
+		// If it returns an error, the client request is aborted with that error value.
+		Got1xxResponse: func(code int, header textproto.MIMEHeader) error {
+			return nil
+		}, // Go 1.11
+
+		// DNSStart is called when a DNS lookup begins.
+		DNSStart: func(httptrace.DNSStartInfo) {
+
+		},
+
+		// DNSDone is called when a DNS lookup ends.
+		DNSDone: func(dnsInfo httptrace.DNSDoneInfo) {
+			//log.Println(spider.CurrentRequest.URL.String(), dnsInfo)
+		},
+
+		// ConnectStart is called when a new connection's Dial begins.
+		// If net.Dialer.DualStack (IPv6 "Happy Eyeballs") support is
+		// enabled, this may be called multiple times.
+		ConnectStart: func(network, addr string) {
+
+		},
+
+		// ConnectDone is called when a new connection's Dial
+		// completes. The provided err indicates whether the
+		// connection completedly successfully.
+		// If net.Dialer.DualStack ("Happy Eyeballs") support is
+		// enabled, this may be called multiple times.
+		ConnectDone: func(network, addr string, err error) {
+
+		},
+
+		// TLSHandshakeStart is called when the TLS handshake is started. When
+		// connecting to a HTTPS site via a HTTP proxy, the handshake happens after
+		// the CONNECT request is processed by the proxy.
+		TLSHandshakeStart: func() {
+
+		}, // Go 1.8
+
+		// TLSHandshakeDone is called after the TLS handshake with either the
+		// successful handshake's connection state, or a non-nil error on handshake
+		// failure.
+		TLSHandshakeDone: func(tls.ConnectionState, error) {
+
+		}, // Go 1.8
+
+		// WroteHeaderField is called after the Transport has written
+		// each request header. At the time of this call the values
+		// might be buffered and not yet written to the network.
+		WroteHeaderField: func(key string, value []string) {
+
+		}, // Go 1.11
+
+		// WroteHeaders is called after the Transport has written
+		// all request headers.
+		WroteHeaders: func() {
+
+		},
+
+		// Wait100Continue is called if the Request specified
+		// "Expected: 100-continue" and the Transport has written the
+		// request headers but is waiting for "100 Continue" from the
+		// server before writing the request body.
+		Wait100Continue: func() {
+
+		},
+
+		// WroteRequest is called with the result of writing the
+		// request and any body. It may be called multiple times
+		// in the case of retried requests.
+		WroteRequest: func(httptrace.WroteRequestInfo) {
+
+		},
 	}
 }
