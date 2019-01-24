ec35eb00662f32bad931e4dd5ab4a509e4faf1fe
chenset <4199191@qq.com>
2019-01-24 23:52:26 +0800

    update

--- src/asuka/main.go
+++ src/asuka/main.go
@@ -32,12 +32,12 @@ func asuka() {
 	fmt.Println("Monitor: http://127.0.0.1:666")
 
 	web.Server([]*project.Dispatcher{
-		project.New(&project.DouBan{}).Run(),
+		//project.New(&project.DouBan{}).Run(),
 		project.New(&project.Test{}).Run(),
 		project.New(&project.Test2{}).Run(),
-		//project.New(&project.ZhiHu{}).Run(),
-		//project.New(&project.JianShu{}).Run(),
-		//project.New(&project.Www{}).Run(),
+		project.New(&project.ZhiHu{}).Run(),
+		project.New(&project.JianShu{}).Run(),
+		project.New(&project.Www{}).Run(),
 
 		//project.New(&project.Test{}).CleanUp().Run(),
 		//project.New(&project.Test2{}).CleanUp().Run(),
