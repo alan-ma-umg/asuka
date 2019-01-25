ad23ad36100f0a5df8229ad509333bf029a1414a
chenset <4199191@qq.com>
2019-01-25 22:55:41 +0800

    update

--- runTest.sh
+++ runTest.sh
@@ -1,15 +1,13 @@
 #! /usr/bin/env bash
 
+killall asuka >/dev/null 2>/dev/null
+
 echo 'Welcome! Asuka'
 git reset HEAD --hard --quiet && git pull --rebase --quiet && go get asuka
 if [ $? -ne 0 ];then
     echo 'Update failed!'
     exit 1;
 fi
-
-killall asuka >/dev/null 2>/dev/null
-echo 'Killed Asuka!'
-
 
 echo 'Rebuild Asuka!'
 go build asuka
