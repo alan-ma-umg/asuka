57b61546d43b61abeff0eadc625dc6e6e32d40f6
chenset <4199191@qq.com>
2019-05-21 19:30:34 +0800

    update

--- main.go
+++ main.go
@@ -4,117 +4,74 @@ import (
 	"flag"
 	"fmt"
 	"github.com/chenset/asuka/helper"
+	"github.com/chenset/asuka/project"
+	"github.com/chenset/asuka/web"
 	"log"
 	"net/url"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	//helper.PathToEnvFile = os.Args[1]
 
-	//C:\data\codes\asuka\env.json
-
-	redis := flag.String("redis", "redis://:password@127.0.0.1:3306/10?timeout=0", "Redis connection url, default: redis://:password@127.0.0.1:6379/10?timeout=0")
-	mysql := flag.String("mysql", "dns://root:11111111@(127.0.0.1:3306)/asuka?charset=utf8mb4", "Mysql DSN, default: dns://root:11111111@(127.0.0.1:3306)/asuka?charset=utf8mb4")
+	redis := flag.String("redis", "tcp://127.0.0.1:6379/10", "Redis connection url, default: tcp://127.0.0.1:6379/10")
+	mysql := flag.String("mysql", "root:11111111@(10.0.0.2:3306)/asuka?charset=utf8mb4", "Mysql DSN, default: root:11111111@(127.0.0.1:3306)/asuka?charset=utf8mb4")
 	webPassword := flag.String("webPassword", "", "WEB login password, default random")
 	bloomFilterPath := flag.String("bloomFilterPath", ".", "BloomFilter save path, default:.")
 	localTransport := flag.Bool("localTransport", false, "Enable http.DefaultTransport, default: false")
-
 	flag.Parse()
-	//if *webPassword == "" {
-	//	rand.Seed(time.Now().Unix())
-	//	flag.Set("webPassword", strconv.Itoa(rand.Int()))
-	//}
+
+	u, err := url.Parse(*redis)
+	if err != nil {
+		log.Fatal(err)
+	}
+	redisDB, _ := strconv.Atoi(strings.TrimLeft(u.Path, "/"))
+	redisPassword, _ := u.User.Password()
 	helper.GlobalEnvConfig = &helper.EnvConfig{
 		BloomFilterPath: *bloomFilterPath,
 		WEBPassword:     *webPassword,
 		LocalTransport:  *localTransport,
 		MysqlDSN:        *mysql,
 		Redis: helper.Redis{
-			Network:     "tcp",
-			Addr:        "127.0.0.1",
-			Password:    "",
-			DB:          10,
+			Network:     u.Scheme,
+			Addr:        u.Host,
+			Password:    redisPassword,
+			DB:          redisDB,
 			URLQueueKey: "url_queue_key",
 		},
 	}
-	//helper.GlobalEnvConfig.BloomFilterPath = *bloomFilterPath
-	//helper.GlobalEnvConfig.WEBPassword = *webPassword
-	//helper.GlobalEnvConfig.LocalTransport = *localTransport
-	//helper.GlobalEnvConfig.MysqlDSN = *mysql
-	//
-	u, err := url.Parse(*redis)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(u.Scheme)
-
-	//Network     string
-	//Addr        string
-	//Password    string
-	//DB          int
-	//URLQueueKey string
-
-	//log.Println(u.Host)
-	//log.Println(u.Hostname())
-	//log.Println(u.User.Password())
-	//log.Println(u.User.Username())
-
-	//helper.GlobalEnvConfig.Redis = *redis
-
-	//log.Println(*redis)
-	//log.Println(*mysql)
-	//log.Println(*webPassword)
-	//log.Println(*bloomFilterPath)
-	//log.Println(*localTransport)
 }
 
 func main() {
 	mainStart := time.Now()
-	//helper.ExitHandle()
+	helper.ExitHandle()
 	defer func() {
 		fmt.Println("Done: ", time.Since(mainStart))
 	}()
 
-	//[: password@] host [: port] [/ database][? [timeout=timeout[d|h|m|s|ms|us|ns]]
-
-	//l := "redis://:password@127.0.0.1:3306/10?timeout=0"
-
-	//u, err := url.Parse(l)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-
-	//log.Println(u.Host)
-	//log.Println(u.Hostname())
-	//log.Println(u.User.Password())
-	//log.Println(u.User.Username())
-
-	//asuka()
+	asuka()
 }
 
-//func asuka() {
-//	fmt.Println("Monitor: http://127.0.0.1:666")
-//
-//	log.Println(web.Server([]*project.Dispatcher{
-//		project.New(&project.DouBan{}).Run(),
-//		//project.New(&project.Pixiv{}).Run(),
-//		//project.New(&project.Test{}).Run(),
-//		//project.New(&project.Test2{}).Run(),
-//		//project.New(&project.ZhiHu{}).Run(),
-//		//project.New(&project.JianShu{}).Run(),
-//		//project.New(&project.Www{}).Run(),
-//
-//		//project.New(&project.Pixiv{}).CleanUp().Run(),
-//		//project.New(&project.DouBan{}).CleanUp().Run(),
-//		//project.New(&project.Test2{}).CleanUp().Run(),
-//		//project.New(&project.ZhiHu{}).CleanUp().Run(),
-//		//project.New(&project.JianShu{}).CleanUp().Run(),
-//		//project.New(&project.Www{}).CleanUp().Run(),
-//	}, ":666")) // http://127.0.0.1:666
-//	helper.ExitHandleFunc()
-//}
+func asuka() {
+	fmt.Println("Monitor: http://127.0.0.1:666")
 
-//2019/01/24 11:56:11 h2_bundle.go:8723: protocol error: received *http.http2GoAwayFrame before a SETTINGS frame
+	log.Println(web.Server([]*project.Dispatcher{
+		project.New(&project.DouBan{}).Run(),
+		//project.New(&project.Pixiv{}).Run(),
+		//project.New(&project.Test{}).Run(),
+		//project.New(&project.Test2{}).Run(),
+		//project.New(&project.ZhiHu{}).Run(),
+		//project.New(&project.JianShu{}).Run(),
+		//project.New(&project.Www{}).Run(),
+
+		//project.New(&project.Pixiv{}).CleanUp().Run(),
+		//project.New(&project.DouBan{}).CleanUp().Run(),
+		//project.New(&project.Test2{}).CleanUp().Run(),
+		//project.New(&project.ZhiHu{}).CleanUp().Run(),
+		//project.New(&project.JianShu{}).CleanUp().Run(),
+		//project.New(&project.Www{}).CleanUp().Run(),
+	}, ":666")) // http://127.0.0.1:666
+	helper.ExitHandleFunc()
+}
