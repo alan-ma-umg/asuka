571ad2df985dc710d5656e41ed5606a5ae0a323c
chenset <4199191@qq.com>
2019-10-21 22:49:41 +0800

    update

---
+++ rerun.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+killall asuka >/dev/null 2>/dev/null
+
+echo 'Welcome! Asuka'
+#git reset HEAD --hard --quiet && git pull --rebase --quiet
+#if [ $? -ne 0 ];then
+#    echo 'Update failed!'
+#    exit 1;
+#fi
+
+echo 'Rebuild Asuka!'
+go build -ldflags "-s -w"
+
+sleep 2
+echo 'Now, Asuka is alive .'
+nohup ./asuka  -listen :666 &
