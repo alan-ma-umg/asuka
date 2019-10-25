cc1038a03ad8e698faebddab31b84d988dc4eb54
chenset <4199191@qq.com>
2019-10-25 23:44:28 +0800

    update

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 		fmt.Println("Done: ", time.Since(mainStart))
 	}()
 
+	//todo WEB输入频率限制
 	//todo mysql/oracle 数据库写入支持
 	//todo 文件日记展示系统
 	//todo chrome 插件 => pixiv.net js自动抓取报送
@@ -38,19 +39,19 @@ func main() {
 func asuka() {
 	fmt.Println("http://127.0.0.1:" + strings.Split(helper.Env().WEBListen, ":")[len(strings.Split(helper.Env().WEBListen, ":"))-1])
 	log.Println(web.Server([]*project.Dispatcher{
-		project.New(&project.DouBan{}, time.Time{}).Run(),
-		project.New(&project.Pixiv{}, time.Time{}).Run(),
+		//project.New(&project.DouBan{}, time.Time{}).Run(),
+		//project.New(&project.Pixiv{}, time.Time{}).Run(),
 		//project.New(&project.Test2{}, time.Time{}).Run(),
 		//project.New(&project.ZhiHu{}, time.Now()).Run(),
 		//project.New(&project.JianShu{}, time.Now()).Run(),
 		//project.New(&project.Www{}, time.Now()).Run(),
-		project.New(&project.Forever{}, time.Time{}).CleanUp().Run(),
+		//project.New(&project.Forever{}, time.Time{}).CleanUp().Run(),
 		//project.New(&project.Pixiv{}).CleanUp().Run(),
 		//project.New(&project.DouBan{}).CleanUp().Run(),
-		project.New(&project.Test2{}, time.Now().Add(time.Minute*10)).CleanUp().Run(),
-		project.New(&project.ZhiHu{}, time.Now().Add(time.Minute*15)).CleanUp().Run(),
-		project.New(&project.JianShu{}, time.Now().Add(time.Minute*5)).CleanUp().Run(),
-		project.New(&project.Www{}, time.Now().Add(time.Minute*20)).CleanUp().Run(),
+		//project.New(&project.Test2{}, time.Now().Add(time.Minute*10)).CleanUp().Run(),
+		//project.New(&project.ZhiHu{}, time.Now().Add(time.Minute*15)).CleanUp().Run(),
+		//project.New(&project.JianShu{}, time.Now().Add(time.Minute*5)).CleanUp().Run(),
+		//project.New(&project.Www{}, time.Now().Add(time.Minute*20)).CleanUp().Run(),
 	}, helper.Env().WEBListen))
 	helper.ExitHandleFunc()
 }
