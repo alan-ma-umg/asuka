c56e4a35c29fa5cc373e9db570bc6a2786d0c66b
chenset <4199191@qq.com>
2019-10-28 16:29:15 +0800

    update

---
+++ kumiko.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+killall kumiko >/dev/null 2>/dev/null
+
+echo 'Welcome! kumiko tcp filter server'
+git reset HEAD --hard --quiet && git pull --rebase --quiet
+if [ $? -ne 0 ];then
+    echo 'Update failed!'
+    exit 1;
+fi
+
+echo 'Rebuild Kumiko!'
+go build -ldflags "-s -w" -o kumiko
+
+sleep 5
+
+nohup ./kumiko  -bloomFilterServer :17654 -bloomFilterClient &
