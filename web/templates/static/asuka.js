1ba643ff584c13de2e88523cf4f025c3a2bc71c4
chenset <4199191@qq.com>
2019-10-31 12:11:19 +0800

    update

--- web/templates/static/asuka.js
+++ web/templates/static/asuka.js
@@ -236,7 +236,7 @@ document.addEventListener('click', function (evt) {
 });
 
 function goToUrl(dstUrl) {
-    if (document.referrer.replace(location.origin, '') === dstUrl.replace(location.origin, '')) {
+    if (document.referrer && document.referrer.startsWith(location.origin) && document.referrer.replace(location.origin, '') === dstUrl.replace(location.origin, '')) {
         history.replaceState(dstUrl, document.title, dstUrl);
         history.back();// Browser cache
     } else {
